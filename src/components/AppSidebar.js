05960122201950d3b85edd7ea6eee8a5bd336c0f
Tharushika Dilmini <dilz2001.1221@gmail.com>
2024-09-11 10:33:11 +0530

    new commits

--- src/components/AppSidebar.js
+++ src/components/AppSidebar.js
@@ -13,9 +13,6 @@ import CIcon from '@coreui/icons-react'
 
 import { AppSidebarNav } from './AppSidebarNav'
 
-import { logo } from 'src/assets/brand/logo'
-import { sygnet } from 'src/assets/brand/sygnet'
-
 // sidebar nav config
 import navigation from '../_nav'
 
@@ -37,7 +34,7 @@ const AppSidebar = () => {
     >
       <CSidebarHeader className="border-bottom">
         <CSidebarBrand to="/" style={{ textDecoration: 'none' }}>
-          <img src="src/assets/resources/city.png" alt="City Taxi Logo" style={{ height: '40px', marginRight: '10px' }} />
+          <img src="src/assets/resources/logo.jpeg" alt="City Taxi Logo" style={{ height: '40px', marginRight: '10px' }} />
           <span style={{ fontWeight: 'bold', fontSize: '18px', color: '#fff'}}>CITY TAXI PVT LTD</span>
         </CSidebarBrand>
         <CCloseButton
