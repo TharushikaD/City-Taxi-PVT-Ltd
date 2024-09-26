e76865b97407d3dad46708db00cb73c2e0f473fa
Tharushika Dilmini <dilz2001.1221@gmail.com>
2024-09-26 22:10:32 +0530

    few modifications

--- src/components/service/Service.jsx
+++ src/components/service/Service.jsx
@@ -1,18 +1,45 @@
-import axios from "axios";
+// import axios from "axios";
 
+
+// const instance = axios.create({
+//     baseURL: 'http://localhost:8080/v1/users', 
+// });
+
+
+// instance.interceptors.request.use(
+//     (config) => {
+//         const token = localStorage.getItem('authToken'); 
+
+//         if (token) {
+//             config.headers['Authorization'] = `Bearer ${token}`;
+//         }
+//         return config;
+//     },
+//     (error) => {
+//         return Promise.reject(error);
+//     }
+// );
+
+
+import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8080/v1/users', 
+    baseURL: 'http://localhost:8080/v1/users',
+    headers: {
+        'Content-Type': 'application/json',  
+    },
+    withCredentials: true  
 });
 
 
 instance.interceptors.request.use(
     (config) => {
-        const token = localStorage.getItem('authToken'); 
+        const token = localStorage.getItem('authToken');  
 
         if (token) {
-            config.headers['Authorization'] = `Bearer ${token}`;
+            config.headers['Authorization'] = `Bearer ${token}`;  
         }
+
         return config;
     },
     (error) => {
@@ -20,4 +47,20 @@ instance.interceptors.request.use(
     }
 );
 
+
+instance.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    (error) => {
+        
+        if (error.response && error.response.status === 401) {
+            
+            console.error('Unauthorized, please log in again.');
+        }
+        return Promise.reject(error);
+    }
+);
+
 export default instance;
+
